7551be9d428bf4069d8833a28828dcc46c1d5e22
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2020-12-12 21:51:31 -0600

    fixed validation, changed innerHTML to a variable input, and added fetch/json

--- script.js
+++ script.js
@@ -1,91 +1,65 @@
 // Write your JavaScript code here!
-window.addEventListener('load', function() {
+window.addEventListener('load', function(event) {
+   document.getElementById("faultyItems").style.visibility = "visible";
    //event handler for button click
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event){
+   event.preventDefault();
+      
       let pilotName = document.querySelector("input[name=pilotName]");
+      //let pilotTest = Number(pilotName);
       let copilotName = document.querySelector("input[name=copilotName]");
-      let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
-      let fuelLevelConvert = parseInt(fuelLevel, 10);
+      //let copilotTest = Number(copilotName);
+      let fuelLevel = (document.querySelector("input[name=fuelLevel]").value)
+     // let fuelLevelTest = Number(fuelLevel);
       let cargoMass = document.querySelector("input[name=cargoMass]").value;
-      let cargoMassConvert = parseInt(cargoMass, 10);
-
+      //let cargoMassTest = Number(cargoMass);
       if (pilotName.value === "" || copilotName.value === "" || fuelLevel === "" || cargoMass === ""){
          alert("All fields required!")
-         event.preventDefault();
+         //event.preventDefault();
       } 
-      //TODO: Fix validation of datatype
-      else if (pilotName.value !== typeof "string" || copilotName.value !== typeof "string" || fuelLevelConvert !== typeof "number" || cargoMassConvert !== typeof "number") {
+      else if (!isNaN(pilotName.value) || !isNaN(copilotName.value) || isNaN(fuelLevel) ||  isNaN(cargoMass)) {
          alert("Incorrect data type!")
-         event.preventDefault();
+         //event.preventDefault();
       };
-
-      console.log(`this is the value pilotName ${pilotName.value}`);
-      console.log(`this is the typeof pilotName ${typeof pilotName.value}`);
-      console.log(`this is the value copilotName ${copilotName.value}`);
-      console.log(`this is the typeof copilotName ${typeof copilotName.value}`);
-
-      console.log("\n");
-
-      console.log(`this is the value of fuelLevelConvert ${fuelLevelConvert}`);
-      console.log(`this is the typeof fuelLevelConvert ${typeof fuelLevelConvert}`);
-      console.log(`this is the value of cargoMassConvert ${cargoMassConvert}`)
-      console.log(`this is the typeof cargoMassConvert ${typeof cargoMassConvert}`)
-
-      console.log("\n");
-
-      console.log(`this is the input value of fuelLevel ${fuelLevel}`);
-      console.log(`this is the typeof fuelLevel ${typeof fuelLevel}`);
-      console.log(`this is the input value of cargoMass ${cargoMass}`);
-      console.log(`this is the typeof cargoMass ${typeof cargoMass}`);
+       console.log(`this is the value pilotName ${pilotName.value}`);
+       console.log(`this is the value copilotName ${copilotName.value}`);
+      // console.log(`this is the input value of fuelLevel ${fuelLevel}`);
+      // console.log(`this is the input value of cargoMass ${cargoMass}`);
+   
+      document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotName.value} Ready`;
+      document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilotName.value} Ready`;
       
    });
 
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+     response.json().then(function(json) {
+        //console.log(json);
+        let randomNum = Math.floor(Math.random() * 6);
+        let planetInfo = json[randomNum];
+       const planet = document.getElementById("missionTarget")
+       planet.innerHTML = `
+       <h2>Mission Destination</h2>
+        <ul id="planetList">
+           <li>Name: ${planetInfo.name} </li>
+           <li>Diameter: ${planetInfo.diameter} </li>
+           <li>Star: ${planetInfo.star} </li>
+           <li>Distance from Earth: ${planetInfo.distance} </li>
+           <li>Number of Moons: ${planetInfo.moons} </li>
+        </ul>
+        <img src="${planetInfo.image}">`
+   
+        
+     });
+   });
+   
+   
+   
+
+   
+   
+   
+   
 });
 
-//    let form = document.querySelector("form");
-//    form.addEventListener("submit", function(event){
-//       let pilotName = document.querySelector("input[name=pilotName]");
-//       let copilotName = document.querySelector("input[name=copilotName]");
-//       let fuelLevel = document.querySelector("input[name=fuelLevel]");
-//       let cargoMass = document.querySelector("input[name=cargoMass]");
-//       if (pilotName.value === "" || pilotName.value !== typeof "string"){
-//          alert("All fields required!")
-//          event.preventDefault();
-//       };
-//       if (copilotName.value === "" || copilotName.value !== typeof "string"){
-//          alert("All fields required!")
-//          event.preventDefault();
-//       };
-//       if (fuelLevel.value === "" || fuelLevel.value !== typeof "number"){
-//          alert("All fields required!")
-//          event.preventDefault();
-//       };
-//       if (cargoMass.value === "" || cargoMass.value !== typeof "number"){
-//          alert("All fields required!")
-//          event.preventDefault();
-//       }
-//    });
-// });
 
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  
-                  /* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
